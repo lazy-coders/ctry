b6555c0f5474c734576434bcc2648160609101b5
srs003es <samuel.rodriguez@diagroup.com>
2014-01-02 14:45:26 +0000

    Adding test cases

---
+++ tests/test01.c
@@ -0,0 +1,14 @@
+#nclude <ctry.h>
+
+int main(void) {
+	CTRY {
+		printf("Throwing error inside CTRY body...");
+		CTHROW(1);
+	}
+	CCATCH {
+		printf("Error catched\n");
+	}
+	CENDTRY;
+
+	return 0;
+}
